d30536935c14cb04d1169eeb7cffb5bd9c3bec54
Roger Puga Ruiz <rogerpugaruiz@gmail.com>
2023-02-10 12:37:29 +0100

    production

--- plastic_detection/settings.py
+++ plastic_detection/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r^4t5+(vg6h0)3hn=88ij2gqwtm27a4@3(hhqa^r&$(+0hv@w9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'map_viewer'
 ]
 
 MIDDLEWARE = [
