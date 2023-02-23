89aaff2ee5eacdcb39d44d5f0ab2bd4c5bdf14e2
Roger Puga Ruiz <rogerpugaruiz@gmail.com>
2023-02-23 08:48:30 +0100

    add logos

---
+++ map_viewer/static/js/load_web.js
@@ -0,0 +1,34 @@
+let loadingText = ""
+let addingChars = ".".repeat(5)
+const element = document.getElementById("loading-dots")
+let charCount = 0;
+let currentString = loadingText
+let timer = 0
+
+function doLoop() {
+    if (charCount >= addingChars.length) {
+        charCount = 0;
+        currentString = loadingText
+    }
+
+    currentString += addingChars[charCount]
+    element.innerText = currentString
+    charCount++
+    timer++
+    if (timer > 3) {
+        // document.getElementById("mapContainer").style.height = "100%"
+        document.getElementById("loading").style.display = "none"
+        document.getElementById("press-any-key").style.display = "block"
+        document.addEventListener("keydown", function(event) {
+            document.getElementById("load-app").style.display = "none"
+
+        })
+
+    } else {
+        console.log(timer)
+        setTimeout(doLoop, 500)
+    }
+
+}
+
+doLoop()
--- plastic_detection/settings.py
+++ plastic_detection/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r^4t5+(vg6h0)3hn=88ij2gqwtm27a4@3(hhqa^r&$(+0hv@w9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -119,9 +120,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    'map_viewer/static',
-]
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'map_viewer', 'static'),
+    ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
